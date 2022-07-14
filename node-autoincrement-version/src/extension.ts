23a201defbbdceb1a35e952e19268ccda93bb224
Amber Horvath <ambear9@gmail.com>
2022-07-14 17:58:05 -0400

    code to update package version number is done

--- node-autoincrement-version/src/extension.ts
+++ node-autoincrement-version/src/extension.ts
@@ -5,20 +5,67 @@ const gitExtension = vscode.extensions.getExtension('vscode.git')?.exports;
 console.log('gitExtension', gitExtension)
 export const gitApi = gitExtension?.getAPI(1);
 console.log('gitApi', gitApi);
+// https://stackoverflow.com/questions/36316326/typescript-ts7015-error-when-accessing-an-enum-using-a-string-type-parameter
+let updateType: string | keyof typeof VERSIONS = vscode.workspace.getConfiguration('version').get('options') ?? 'patch' ;
+console.log('updateType', updateType)
+
+enum VERSIONS {
+	'major',
+	'minor',
+	'patch'
+}
 
 const getPackageJsonFilePath = async () => {
 	return await vscode.workspace.findFiles('**/package.json', '**/node_modules/**/package.json');
-}
-
-const updatePackageJson = async () => {
-	// The code you place here will be executed every time your command is executed
-	// Display a message box to the user
-	const packageJsonUri: vscode.Uri = (await getPackageJsonFilePath())[0];
-	const packageJsonDocument = (await vscode.workspace.openTextDocument(packageJsonUri));
-	let text = packageJsonDocument.getText();
-	console.log('packageJsonDocument', text)
 };
 
+// TODO: may not work if there is somehow a version number that is the same as the one that we are editing
+// and it appears before the real package.json version number
+const getRangeOfSubstring = (text: string, substring: string) : vscode.Range => {
+	const firstIndex = text.indexOf(substring);
+	const sub = text.substring(0, firstIndex);
+	const numLines = sub.split('\n').length - 1; // vscode is 0-indexed for line numbers
+	const subsub = sub.substring(sub.lastIndexOf('\n'));
+	const startOffset = subsub.length - 1;
+	const endOffset = startOffset + substring.length;
+	return new vscode.Range(new vscode.Position(numLines, startOffset), new vscode.Position(numLines, endOffset));
+};
+
+const updatePackageJson = async () => {
+	const packageJsonUri: vscode.Uri = (await getPackageJsonFilePath())[0];
+	const packageJsonDocument = (await vscode.workspace.openTextDocument(packageJsonUri));
+	let rawText = packageJsonDocument.getText();
+	let text = JSON.parse(rawText);
+	let versionText = text.version;
+	let updatedVersion = incrementVersionNumber(versionText);
+	text.version = updatedVersion;
+	const wsEdit = new vscode.WorkspaceEdit();
+	wsEdit.replace(packageJsonUri, getRangeOfSubstring(rawText, versionText), updatedVersion);
+	vscode.workspace.applyEdit(wsEdit);
+};
+
+const incrementValue = (indexedString: string) : string => {
+	return `${parseInt(indexedString) + 1}`;
+}
+
+// TODO: validation for package version number (assuming x.x.x format, but not 1000% sure)
+const incrementVersionNumber = (currentVersion: string) : string => {
+	let splitVersion: string[] = currentVersion.split('.');
+	splitVersion[VERSIONS[updateType as keyof typeof VERSIONS]] = incrementValue(splitVersion[VERSIONS[updateType as keyof typeof VERSIONS]]);
+	switch(updateType) {
+		case 'major':
+			splitVersion[VERSIONS['minor']] = '0';
+		case 'minor':
+			splitVersion[VERSIONS['patch']] = '0';
+	}
+	console.log('bazinga',  splitVersion.join('.'));
+	return splitVersion.join('.');
+};
+
+const configDidChange = () => {
+	updateType = vscode.workspace.getConfiguration('version').get('options') ?? 'patch';
+	console.log('new update', updateType);
+};
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -35,6 +82,8 @@ export async function activate(context: vscode.ExtensionContext) {
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('node-autoincrement-version.helloWorld', updatePackageJson);
+	
+	context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(() => configDidChange()));
 
 	context.subscriptions.push(disposable);
 }
